17825e468f20ae514819fb02f8e11357ba6edca0
LittleIcyIcy <20164522@stu.neu.edu.cn>
2019-07-25 13:43:58 +0800

    创建对应的ViewModel,并将View与ViewModel连接起来

--- ChooseFood/ChooseFood/ViewModelLocator.cs
+++ ChooseFood/ChooseFood/ViewModelLocator.cs
@@ -15,10 +15,34 @@ namespace ChooseFood
         public MainPageViewModel MainPageViewModel =>
             SimpleIoc.Default.GetInstance<MainPageViewModel>();
 
+        public MenuPage1ViewModel MenuPage1ViewModel =>
+            SimpleIoc.Default.GetInstance<MenuPage1ViewModel>();
+
+        public MenuPage2ViewModel MenuPage2ViewModel =>
+            SimpleIoc.Default.GetInstance<MenuPage2ViewModel>();
+
+        public MenuPage3ViewModel MenuPage3ViewModel =>
+            SimpleIoc.Default.GetInstance<MenuPage3ViewModel>();
+
+        public MenuPage4ViewModel MenuPage4ViewModel =>
+            SimpleIoc.Default.GetInstance<MenuPage4ViewModel>();
+
+        public LikePageViewModel LikePageViewModel =>
+            SimpleIoc.Default.GetInstance<LikePageViewModel>();
+
+        public DislikePageViewModel DislikePageViewModel =>
+            SimpleIoc.Default.GetInstance<DislikePageViewModel>();
+
         public ViewModelLocator()
         {
             SimpleIoc.Default.Register<ILoadJsonService, LoadJsonService>();
             SimpleIoc.Default.Register<MainPageViewModel>();
+            SimpleIoc.Default.Register<MenuPage1ViewModel>();
+            SimpleIoc.Default.Register<MenuPage2ViewModel>();
+            SimpleIoc.Default.Register<MenuPage3ViewModel>();
+            SimpleIoc.Default.Register<MenuPage4ViewModel>();
+            SimpleIoc.Default.Register<LikePageViewModel>();
+            SimpleIoc.Default.Register<DislikePageViewModel>();
         }
     }
 }
---
+++ ChooseFood/FoodLibrary/ViewModels/MenuPage4ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace FoodLibrary.ViewModels
+{
+    public class MenuPage4ViewModel:ViewModelBase
+    {
+    }
+}
