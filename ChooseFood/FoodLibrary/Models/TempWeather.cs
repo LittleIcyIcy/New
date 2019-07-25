481ff42a38fef4ffb8cdc7a3ca4d902ad2d30c97
LittleIcyIcy <20164522@stu.neu.edu.cn>
2019-07-25 13:52:14 +0800

    Model里面添加了所用的类

---
+++ ChooseFood/FoodLibrary/Models/TempWeather.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodLibrary.Models
+{
+    public class TempWeather
+    {
+        public Temperature Temperature; //{ get; set; }
+        public Humidity Humidity; //{ get; set; }
+    }
+    public enum Temperature
+    {
+        Low = 0,
+        Medium = 1,
+        Hot = 2,
+    }
+    public enum Humidity
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2
+    }
+    public static class DoubleMethods
+    {
+        public static Temperature ToTemperature(this Double d)
+        {
+            if (d < 0)
+            {
+                return Temperature.Low;
+            }
+            else if (d > 30)
+            {
+                return Temperature.Hot;
+            }
+            else
+            {
+                return Temperature.Medium;
+            }
+        }
+        public static Humidity ToHumidity(this Double d)
+        {
+            if (d < 30)
+            {
+                return Humidity.Low;
+            }
+            else if (d < 60)
+            {
+                return Humidity.Medium;
+            }
+            else
+            {
+                return Humidity.High;
+            }
+        }
+    }
+}
---
+++ ChooseFood/FoodLibrary/Models/Vector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodLibrary.Models
+{
+    public class Vector
+    {
+        public int Temperature;
+        public int Humidity;
+    }
+}
